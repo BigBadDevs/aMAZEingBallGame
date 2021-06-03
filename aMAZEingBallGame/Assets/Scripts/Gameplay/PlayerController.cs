856894f95231f2b969b09cca71103cb7e3a02f37
Jacaab <60708181+Jacaab@users.noreply.github.com>
2021-06-03 15:35:49 +0100

    jump
    jump feature is primitive but functional. public variables for adjustments need finalised as private. broken floor collisions as a result

--- aMAZEingBallGame/Assets/Scripts/Gameplay/PlayerController.cs
+++ aMAZEingBallGame/Assets/Scripts/Gameplay/PlayerController.cs
@@ -12,24 +12,42 @@ public class PlayerController : MonoBehaviour
     public int scoreCount;
     public int retryCount;
 
-    private Collider[] ballCollisions;
+    // use this to adjust how much the player can jump and then make it private
+    public int jumpHeight;
 
+    private Collider[] ballCollisions;
+    private float distToGround;
+
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
 
     void Start()
 	{
         scoreCount = 0;
         retryCount = 0;
         rb = gameObject.GetComponent<Rigidbody>();
+        distToGround = GetComponent<Collider>().bounds.extents.y;
     }
 	
 	void Update ()
 	{
+        // in the input manager you can change these for mobile inputs like touch swipe or tilt
+        // basic xz movement
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized;
 
         rb.AddForce(movement * speed * Time.deltaTime);
+
+        // jump with button input
+        if (Input.GetButton("Jump")&& IsGrounded())
+        {
+            rb.AddForce(0f, jumpHeight, 0f);
+        }
 
         ballCollisions = Physics.OverlapSphere(gameObject.transform.position, 0.78f);     // search for a player coliding with the ball
         foreach (Collider index in ballCollisions)
