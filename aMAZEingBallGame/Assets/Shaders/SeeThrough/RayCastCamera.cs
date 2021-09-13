8cefed72f5aa079a2fcdbe87a31c02195477412f
Ivan <46633666+ivanchotom@users.noreply.github.com>
2021-09-13 16:36:33 +0300

    LevelDesign

--- aMAZEingBallGame/Assets/Shaders/SeeThrough/RayCastCamera.cs
+++ aMAZEingBallGame/Assets/Shaders/SeeThrough/RayCastCamera.cs
@@ -1,29 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class RayCastCamera : MonoBehaviour
 {
     public GameObject camera;
     public GameObject target;
-    public LayerMask mylayermask;
+    public GameObject mask;
+    //public LayerMask mylayermask;
 
 
     // Update is called once per frame
     void Update()
     {
         RaycastHit hit;
-        if(Physics.Raycast(camera.transform.position, (target.transform.position - camera.transform.position).normalized,
-            out hit, Mathf.Infinity, mylayermask))
+        if(Physics.Raycast(camera.transform.position, 
+            (target.transform.position - camera.transform.position).normalized,
+            out hit, Mathf.Infinity))
         {
 
             if (hit.collider.gameObject.tag == "spheremask")
             {
+                mask.transform.DOScale(0, 2);
                 Debug.Log("See");
             }
 
             else
             {
+                mask.transform.DOScale(6, 2);
                 Debug.Log("NOTSee");
             }
         }
